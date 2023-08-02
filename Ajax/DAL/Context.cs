d3267c4b2fadae54798f864edc758830a49fb84d
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-08-03 01:24:23 +0300

    Add project files.

---
+++ Ajax/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ajax.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server = MSI; database = AjaxDB; integrated security = true; trusted_connection = true; encrypt = false");
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
